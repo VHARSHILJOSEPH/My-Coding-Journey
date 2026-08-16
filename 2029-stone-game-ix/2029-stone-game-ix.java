64e7e579a608213280f9ee692843e164e80ff305
VHARSHILJOSEPH <harshiljosephvijjeswarapu@gmail.com>
2026-08-16 22:28:40 +0530

    Time: 4 ms (75.68%), Space: 115 MB (16.22%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.java
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -4,7 +4,7 @@ class Solution {
         for(int s:stones){
             f[s%3]++;
         }
-        if((f[0] & 1) ==0){
+        if((f[0] % 2) ==0){
             return Math.min(f[1],f[2])>0;
         }
         return Math.abs(f[1]-f[2])>2;        
