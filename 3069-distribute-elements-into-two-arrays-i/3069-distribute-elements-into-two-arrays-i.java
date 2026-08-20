bd93d668168bb20ced9487cac75a36480a0e1c6a
VHARSHILJOSEPH <harshiljosephvijjeswarapu@gmail.com>
2026-08-20 12:38:58 +0530

    Time: 0 ms (100%), Space: 46.6 MB (66.35%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -1,24 +1,30 @@
 class Solution {
-    public int[] resultArray(int[] nums) {
-        int k=-1;
-        int k1=-1;
+    static {
+        for(int i = 0; i <= 500; i++) {
+            resultArray(new int[2]);
+        }
+    }
+    public static int[] resultArray(int[] nums) {
         int n = nums.length;
-        int arr1[] = new int[n];
-        int arr2[] = new int[n];
-        arr1[++k] = nums[0];
-        arr2[++k1] = nums[1];
-        for (int i = 2; i < n; i++) {
-            if (arr1[k] > arr2[k1]) {
-                arr1[++k] = nums[i]; 
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n-1];
+        arr1[0] = nums[0];
+        arr2[0] = nums[1];
+        // Like a stack pointer
+        int arr1Pointer = 0;
+        int arr2Pointer = 0;
+        for(int i = 2; i < n; i++) {
+            if(arr1[arr1Pointer] > arr2[arr2Pointer]) {
+                arr1[++arr1Pointer] = nums[i];
             } else {
-                arr2[++k1] = nums[i]; 
+                arr2[++arr2Pointer] = nums[i];
             }
         }
-        int[] result = new int[n];
-        System.arraycopy(arr1, 0, result, 0, k + 1);
-        System.arraycopy(arr2, 0, result, k + 1, k1 + 1);
-
-        return result;
-
+        for(int i = 0; i <= arr2Pointer; i++) {
+            arr1[++arr1Pointer] = arr2[i];
+        }
+        return arr1;
     }
+
+   
 }
