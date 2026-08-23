ecd5e35e1b08280121ff6da18491db466488be92
VHARSHILJOSEPH <harshiljosephvijjeswarapu@gmail.com>
2026-08-24 00:22:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -1,31 +1,53 @@
 class Solution {
     public boolean sumGame(String num) {
-        int n = num.length();
-        int mid = n / 2;
+        int half = num.length() >> 1;
+        int diff = 0;
+        int q = 0;
 
-        int leftSum = 0, rightSum = 0;
-        int leftQ = 0, rightQ = 0;
-
-        for (int i = 0; i < mid; i++) {
+        for (int i = 0, j = half; i < half; i++, j++) {
             char l = num.charAt(i);
-            char r = num.charAt(i + mid);
+            char r = num.charAt(j);
 
             if (l == '?')
-                leftQ++;
+                q++;
             else
-                leftSum += l - '0';
+                diff += l - '0';
 
             if (r == '?')
-                rightQ++;
+                q--;
             else
-                rightSum += r - '0';
+                diff -= r - '0';
         }
-
-        int q = leftQ - rightQ;
 
         if ((q & 1) != 0)
             return true;
 
-        return leftSum - rightSum != -(q / 2) * 9;
+        return diff + 9 * (q >> 1) != 0;
+    }
+}class Solution {
+    public boolean sumGame(String num) {
+        int half = num.length() >> 1;
+        int diff = 0;
+        int q = 0;
+
+        for (int i = 0, j = half; i < half; i++, j++) {
+            char l = num.charAt(i);
+            char r = num.charAt(j);
+
+            if (l == '?')
+                q++;
+            else
+                diff += l - '0';
+
+            if (r == '?')
+                q--;
+            else
+                diff -= r - '0';
+        }
+
+        if ((q & 1) != 0)
+            return true;
+
+        return diff + 9 * (q >> 1) != 0;
     }
 }
