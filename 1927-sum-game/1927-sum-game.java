e3760985446c9db9bc697cdd7da8aeacc84df090
VHARSHILJOSEPH <harshiljosephvijjeswarapu@gmail.com>
2026-08-24 00:20:35 +0530

    Time: 8 ms (53.28%), Space: 47.2 MB (10.95%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -1,16 +1,12 @@
 class Solution {
     public boolean sumGame(String num) {
-
         int n = num.length();
         int mid = n / 2;
 
-        int leftSum = 0;
-        int rightSum = 0;
-        int leftQ = 0;
-        int rightQ = 0;
+        int leftSum = 0, rightSum = 0;
+        int leftQ = 0, rightQ = 0;
 
         for (int i = 0; i < mid; i++) {
-
             char l = num.charAt(i);
             char r = num.charAt(i + mid);
 
@@ -25,14 +21,11 @@ class Solution {
                 rightSum += r - '0';
         }
 
-        int diff = leftSum - rightSum;
         int q = leftQ - rightQ;
 
-        // Odd number of unmatched '?'
-        if (q % 2 != 0)
+        if ((q & 1) != 0)
             return true;
 
-        // Mathematical condition
-        return diff != -(q / 2) * 9;
+        return leftSum - rightSum != -(q / 2) * 9;
     }
 }
