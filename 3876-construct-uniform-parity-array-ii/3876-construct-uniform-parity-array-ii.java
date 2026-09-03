30aa882d1aab0fe48caf5e42518c8013dbd4d62b
VHARSHILJOSEPH <harshiljosephvijjeswarapu@gmail.com>
2026-09-03 18:35:24 +0530

    Time: 84 ms (5.07%), Space: 126.6 MB (5.07%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int ans[]=new int [nums1.length];
+        List<Integer> even=new ArrayList<>();
+        List<Integer> odd=new ArrayList<>();
+        for(int n:nums1){
+            if(n%2==0)  even.add(n);            
+            else    odd.add(n);
+        }
+        if(even.size()==nums1.length)    return true;
+        if(odd.size()==nums1.length)    return true;
+        Collections.sort(even);
+        Collections.sort(odd);
+        int evens=even.get(0);
+        int odds=odd.get(0);
+        int i=1,j=1;
+        for(int n :even){
+            while(n-odds<=0){
+                odds++;
+                j++;
+                if(odd.size()<=j){
+                    return false;
+                }
+
+            }
+        }
+        if(evens!=0 && odds==0)   return true;
+        if(evens==0 && odds!=0)   return true;
+        if(evens!=0 && odds!=0)   return true;
+        return false;    
+
+        
+    }
+}
