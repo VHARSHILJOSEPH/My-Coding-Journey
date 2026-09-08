daee13662df7a1d25ce7d7ab8364f643aad181ff
VHARSHILJOSEPH <harshiljosephvijjeswarapu@gmail.com>
2026-09-09 00:06:16 +0530

    Time: 29 ms (22.28%), Space: 47.1 MB (10.93%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,21 +1,36 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length()>s2.length()) return false;
-        int count1[]=new int [26];
-        int count2[]=new int [26];
-        for(char c:s1.toCharArray()){
-            count1[c-'a']++;
+        if (s1.length() > s2.length()) return false;
+
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
+
+        for (char c : s1.toCharArray()) {
+            map1.put(c, map1.getOrDefault(c, 0) + 1);
         }
-        int l=0;
-        for(int r=0;r<s2.length();r++){
-            count2[s2.charAt(r)-'a']++;
-            if(r-l+1>s1.length()){
-                count2[s2.charAt(l)-'a']--;
+
+        int l = 0;
+
+        for (int r = 0; r < s2.length(); r++) {
+            char c = s2.charAt(r);
+            map2.put(c, map2.getOrDefault(c, 0) + 1);
+
+            if (r - l + 1 > s1.length()) {
+                char left = s2.charAt(l);
+                map2.put(left, map2.get(left) - 1);
+
+                if (map2.get(left) == 0) {
+                    map2.remove(left);
+                }
+
                 l++;
             }
-            if(Arrays.equals(count1,count2))   return true;
-        }
-        return false;
 
+            if (map1.equals(map2)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
